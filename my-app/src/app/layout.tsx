b54b62c644435409c1a10273aaf75d323b815beb
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2025-02-24 11:23:03 -0500

    changes

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,10 +1,10 @@
-import "./globals.css";
+import './globals.css';
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <main>
+      <h1>Hello, Next.js!</h1>
+    </main>
+  );
 }
