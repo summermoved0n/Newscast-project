f53a0099db4fd9640b898f36c86f5d202671b86f
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2025-02-24 13:47:59 -0500

    added fonts

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css';
+import { Oswald } from 'next/font/google';
+
+const font = Oswald({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -7,7 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Sofia_Sans } from 'next/font/google';
+
+const font = Sofia_Sans({ subsets: ['latin'] });
+
 export default function Page() {
   return (
     <main>
-      <h1>Hello, Next.js!</h1>
+      <h1 className={font.className}>
+        Indonesia says located black box recorders from crashed plane
+      </h1>
     </main>
   );
 }
